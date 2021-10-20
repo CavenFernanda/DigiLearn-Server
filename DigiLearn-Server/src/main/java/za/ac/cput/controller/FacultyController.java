fad170b477e80a6fff510e9832c3541a86f3c048
Caven Fernanda <fernanda17caven@gmail.com>
2021-10-20 20:19:31 +0200

    Completed university and faculty backend back end

---
+++ DigiLearn-Server/src/main/java/za/ac/cput/controller/FacultyController.java
@@ -0,0 +1,35 @@
+package za.ac.cput.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.entity.Lecturer;
+import za.ac.cput.entity.University;
+import za.ac.cput.service.UniversityService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/faculty")
+public class FacultyController {
+
+    @Autowired
+    private UniversityService universityService;
+
+    @PostMapping("/create")
+    public University create(@RequestBody University lecturer){ return universityService.create(lecturer);}
+
+    @GetMapping("/read")
+    public University read(@PathVariable String id){return universityService.read(id);}
+
+    @PostMapping("/update")
+    public University update(@RequestBody University university){return universityService.update(university);}
+
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable String id){return universityService.delete(id);}
+
+    @GetMapping("/getall")
+    public List<University> getAll()
+    {
+        return universityService.findAll();
+    }
+}
