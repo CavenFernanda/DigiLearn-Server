d3d3179aaf0ac7b0f3c4c5507ad8db77f91c8528
Caven Fernanda <fernanda17caven@gmail.com>
2021-10-20 20:51:51 +0200

    factory Test

---
+++ DigiLearn-Server/src/test/java/za/ac/cput/controller/UniversityControllerTest.java
@@ -0,0 +1,23 @@
+package za.ac.cput.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniversityControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void create() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void read() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+
+}
